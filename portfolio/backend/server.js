31109a9ec353287bc03c6586c046b2fa4b076123
Roland550 <133446553+Roland550@users.noreply.github.com>
2023-05-29 08:29:27 +0100

    starter-project

---
+++ portfolio/backend/server.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+
+const app = express()
+
+const PORT = process.env.PORT || 5000;
+
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//Routes 
+app.get("/", (req, res) => {
+    res.send("Home Page");
+})
+
+// Conect to DB and start serveer
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+        console.log('Server Running on port',+ PORT)
+    }) 
+  })
+  .catch((err) => console.log(err))
